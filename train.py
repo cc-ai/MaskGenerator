8cb9a165cae9726cde6473d6a302d2997a0b6dc2
vict0rsch <vsch@pm.me>
2020-04-08 10:02:42 -0400

    clean up

--- train.py
+++ train.py
@@ -6,18 +6,17 @@ from utils import load_opts, set_mode, prepare_sub_folder, create_model, avg_dur
 from data.datasets import get_loader
 from collections import deque
 
-# from data import CreateDataLoader
-# from models import create_model
-# from util.visualizer import Visualizer
-
 if __name__ == "__main__":
     root = Path(__file__).parent.resolve()
     opt_file = "shared/feature_pixelDA.yml"
-
     opts = load_opts(path=root / opt_file, default=root / "shared/defaults.yml")
+    opts = set_mode("train", opts)
+
     comet_exp = Experiment(
         workspace=opts.comet.workspace, project_name=opts.comet.project_name
     )
+    comet_exp.log_asset(file_data=str(root / opt_file), file_name=root / opt_file)
+    comet_exp.log_parameters(opts)
 
     # ! important to do test first
     val_opt = set_mode("test", opts)
@@ -26,23 +25,12 @@ if __name__ == "__main__":
         Dict(iter(val_loader).next()) for i in range(opts.comet.display_size)
     ]
 
-    opts = set_mode("train", opts)
-    loader = get_loader(opts, real=True)
+    train_loader = get_loader(opts, real=True)
     train_display_images = [
-        Dict(iter(loader).next()) for i in range(opts.comet.display_size)
+        Dict(iter(train_loader).next()) for i in range(opts.comet.display_size)
     ]
 
-    dataset_size = len(loader)
-    print("#training images = %d" % dataset_size)
-
-    if comet_exp is not None:
-        comet_exp.log_asset(file_data=str(root / opt_file), file_name=root / opt_file)
-        comet_exp.log_parameters(opts)
-
-    checkpoint_directory, image_directory = prepare_sub_folder(opts.train.output_dir)
-
     opts.comet.exp = comet_exp
-
     model = create_model(opts)
     model.setup()
 
@@ -50,9 +38,10 @@ if __name__ == "__main__":
     times = deque([0], maxlen=100)
     batch_size = opts.data.loaders.batch_size
     time_str = "Average time per sample at step {}: {:.3f}"
+    checkpoint_directory, image_directory = prepare_sub_folder(opts.train.output_dir)
 
     for epoch in range(opts.train.epochs):
-        for i, data in enumerate(loader):
+        for i, data in enumerate(train_loader):
             times.append(time())
             total_steps += batch_size
 
