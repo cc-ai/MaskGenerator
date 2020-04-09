7eda74bfca0281c1d8ef8f89321538ec534a345c
vict0rsch <vsch@pm.me>
2020-04-09 10:18:14 -0400

    remove comment

--- train.py
+++ train.py
@@ -72,16 +72,3 @@ if __name__ == "__main__":
 
                 iter_data_time = time.time()
 
-        """
-        if epoch % opt.save_epoch_freq == 0:
-            print("saving the model at the end of epoch %d, iters %d" % (epoch, total_steps))
-            model.save_networks("latest")
-            model.save_networks(epoch)
-
-        print(
-            "End of epoch %d / %d \t Time Taken: %d sec"
-            % (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time)
-        )
-        model.update_learning_rate()
-        """
-
